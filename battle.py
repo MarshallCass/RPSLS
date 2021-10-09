93917a468ea35c3b41556c7caa59d7e3c62841e3
Ian Vomero <ivomero@ymail.com>
2021-10-09 10:59:23 -0400

    added starting point on player 1 selection as well as notes.

--- battle.py
+++ battle.py
@@ -1,19 +1,34 @@
+from gestures import Gestures
 from human import Human
 from computer import Computer
 from players import Players
+from gestures import Gestures
 
 
-class Battle:
+class Battle(Gestures):
     def __init__(self):
         self.players_list = []
         self.computers_list = []
+        self.player_1_selection = []
+        self.player_2_selection = []
+        self.computer_selection = []
+    # players_list should pass in one or 2 players
+    # computers_list should pass in 0 or 1 computer
+    # player_1_selection should pass in rock, paper, scissors, lizard, or spock
+    # player_2_selection should pass in rock, paper, scissors, lizard, or spock
+    # player_1 _selection should pass in rock, paper, scissors, lizard, or spock
+
+    def display_welcome(self):
+        print("Welcome to Rock Paper Scissors Lizard Spock!")
 
     def run_game(self):
-
+        self.display_welcome()
         self.players = Players()
         self.players.create_player()
         self.players.create_computer()
-        self.player_input()
+        self.player_1_gesture_selection()
+        # self.player_1_gesture_selction is being called from the parent class in gestures.py. it is giving errors at the moment.
+        # once we get player_1_gesture_selction appended to self.player_1_selction above. We can do the same thing for player 2's selection and the computer selction.
 
 
 # Game player selection validation
--- gestures.py
+++ gestures.py
@@ -1,3 +1,24 @@
 class Gestures:
     def __init__(self) -> None:
+        self.gesture_input = False
         pass
+
+    def player_1_gesture_selection(self):
+        self.gesture_selection = int(
+            input('Player 1 choose: Rock-0, Paper-1, Scissors-2, Lizard-3, Spock-4'))
+        while self.gesture_input != True:
+            if self.gesture_selection.isdigit(range(0, 4)):
+                self.gesture_input = True
+                print('this is ok number')
+            else:
+                print('Your selection was invalid')
+        if self.gesture_selection == 0:
+            self.player_1_selection.appead("Rock")
+        elif self.gesture_selection == 1:
+            self.player_1_selection.appead("Paper")
+        elif self.gesture_selection == 2:
+            self.player_1_selection.appead("Scissors")
+        elif self.gesture_selection == 3:
+            self.player_1_selection.appead("Lizard")
+        elif self.gesture_selection == 4:
+            self.player_1_selection.appead("Spock")
