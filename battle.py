b9a24fbf675131c9063d49d3965303859c816dab
Marshall C <MCass2101@gmail.com>
2021-10-11 07:36:24 -0400

    Merged two versions and commented out so can be reviewed

--- battle.py
+++ battle.py
@@ -5,21 +5,26 @@ from players import Players
 import random
 
 
-
 class Battle(Gestures):
     def __init__(self):
         self.players_list = []
         self.computers_list = []
-        self.human_selection = ()
+        self.player_1_selection = []
+        self.player_2_selection = []
+        self.computer_selection = []
         self.computer_points = 0
-        self.player_one_points = 0
-        self.player_two_points = 0
+        self.player_1_points = 0
+        self.player_2_points = 0
 
     def run_game(self):
+        self.display_welcome()
         self.players = Players()
         self.players.create_player()
         self.players.create_computer()
-        self.game_selection()
+        self.player_input()
+
+    def display_welcome(self):
+        print("Welcome to Rock Paper Scissors Lizard Spock!")
 
 # ### Game play logic
 # # # Rock crushes Scissors 
@@ -32,60 +37,6 @@ class Battle(Gestures):
 # # # Lizard poisons Spock 
 # # # Scissors decapitates Lizard 
 # # # Scissors cuts Paper 
-
-    def game_selection(self):
-        self.player_selection = input("How many players do you have?")
-        
-        if self.human_selection=="rock" or "0" : 0
-        elif self.human_selection=="Spock" or "1" : 1
-        elif self.human_selection=="paper" or "2" : 2
-        elif self.human_selection=="lizard" or "3" : 3
-        elif self.human_selection=="scissors" or "4" : 4
-        else: print("Error on name.")
-   
-
-        if self.player_selection == "1":
-           while self.computer_points and self.player_one_points < 2:
-            print("Please enter your gesture name or number!")
-            self.human_selection = input("0 - rock , 1 - spock, 2 - paper, 3 - lizard, 4 - scissors ")
-            self.computer_selection = random.randint(0,4)
-            self.number_difference = (int(self.computer_selection) - int(self.human_selection)) % 5   
-            if self.number_difference == 0:
-                   print("Tie try again!")
-            elif self.number_difference == 1 or self.number_difference == 2:
-                    self.computer_points + 1  
-                    print("Computer Wins") 
-            elif self.number_difference == 3 or self.number_difference == 4:
-                    self.player_one_points + 1
-                    print("Human Wins")
-        elif self.player_selection == "2":
-            print("Player 1 enter your gesture name or number!")
-            self.player_one_selection = input("0 - rock , 1 - spock, 2 - paper, 3 - lizard, 4 - scissors ")
-            print("Player 2 enter your gesture name or number!")
-            self.player_two_selection = input("0 - rock , 1 - spock, 2 - paper, 3 - lizard, 4 - scissors ")
-            self.number_difference = (int(self.player_two_selection) - int(self.player_one_selection)) % 5
-            if self.number_difference == 0:
-                   print("Tie try again!")
-            elif self.number_difference == 1 or self.number_difference == 2:
-                    self.player_one_points + 1  
-                    print("Player 1 Wins") 
-            elif self.number_difference == 3 or self.number_difference == 4:
-                    self.player_two_points + 1
-                    print("Player 2 Wins")
-        self.player_1_selection = []
-        self.player_2_selection = []
-        self.computer_selection = []
-
-    def display_welcome(self):
-        print("Welcome to Rock Paper Scissors Lizard Spock!")
-
-    def run_game(self):
-        self.display_welcome()
-        self.players = Players()
-        self.players.create_player()
-        self.players.create_computer()
-        self.player_input()
-        self.display_welcome()
 
     def player_input(self):
         selection = False
@@ -101,16 +52,36 @@ class Battle(Gestures):
             else:
                 print("not valid input")
 
-          # run input again
-
     def player_vs_player(self):
         self.player_1_gesture_selection()
         self.player_2_gesture_selection()
 
-        pass
+        while self.player_1_points and self.player_2_points < 2:
+
+            self.number_difference = (int(self.player_1_gesture_selection) - int(self.player_2_gesture_selection)) % 5
+
+            if self.number_difference == 0:
+                   print("Tie try again!")
+            elif self.number_difference == 1 or self.number_difference == 2:
+                    self.player_one_points + 1  
+                    print("Player 1 Wins") 
+            elif self.number_difference == 3 or self.number_difference == 4:
+                    self.player_two_points + 1
+                    print("Player 2 Wins")
 
     def player_vs_computer(self):
         self.player_1_gesture_selection()
         self.computer_gesture_selection()
 
-        pass
+        while self.computer_points and self.player_1_points < 2:
+            self.number_difference = (int(self.computer_gesture_selection) - int(self.player_1_gesture_selection)) % 5   
+
+            if self.number_difference == 0:
+                   print("Tie try again!")
+            elif self.number_difference == 1 or self.number_difference == 2:
+                    self.computer_points + 1  
+                    print("Computer Wins") 
+            elif self.number_difference == 3 or self.number_difference == 4:
+                    self.player_one_points + 1
+                    print("Human Wins")
+
--- gestures.py
+++ gestures.py
@@ -6,16 +6,6 @@ class Gestures:
         self.gesture_to_number = 0
         self.number_to_gesture = ""
 
-##    def picking_gesture(self):
-
-    # def gesture_to_num(self, human_selection):
-        # self.gesture_to_number = self.human_selection()
-        # if self.gesture_to_number=="rock" or "0": return 0
-        # elif self.gesture_to_number=="Spock" or "1": return 1
-        # elif self.gesture_to_number=="paper" or "2": return 2
-        # elif self.gesture_to_number=="lizard" or "3": return 3
-        # elif self.gesture_to_number=="scissors" or "4": return 4
-        # else: print("Error on name.")
     # def number_to_guesture(self):
     #     if self.number_to_gesture==0: return "rock"
     #     elif self.number_to_gesture==1: return "Spock"
@@ -23,18 +13,16 @@ class Gestures:
     #     elif self.number_to_gesture==3: return "lizard"
     #     elif self.number_to_gesture==4: return "scissors"
     #     else: print("Error on number." )
-    # def __init__(self) -> None:
-    #     self.gesture_input = False
-    #     pass
 
     def player_1_gesture_selection(self):
+        # print("Please enter your gesture name or number!")
+        # self.player_1_selection = input("0 - rock , 1 - spock, 2 - paper, 3 - lizard, 4 - scissors ")
         self.gesture_input = False
         self.gesture_selection = int(
             input('Player 1 choose: Rock-0, Paper-1, Scissors-2, Lizard-3, Spock-4'))
         while self.gesture_input != True:
             if self.gesture_selection == 0 or self.gesture_selection == 1 or self.gesture_selection == 2 or self.gesture_selection == 3 or self.gesture_selection == 4:
                 self.gesture_input = True
-                print('this is ok number')
             else:
                 print('Your selection was invalid')
         if self.gesture_selection == 0:
@@ -48,14 +36,31 @@ class Gestures:
         elif self.gesture_selection == 4:
             self.player_1_selection.append("Spock")
 
+        # if self.player_1_gesture_selection =="rock" or "0" : 0
+        # elif self.player_1_gesture_selection =="Spock" or "1" : 1
+        # elif self.player_1_gesture_selection =="paper" or "2" : 2
+        # elif self.player_1_gesture_selection =="lizard" or "3" : 3
+        # elif self.player_1_gesture_selection =="scissors" or "4" : 4
+        # else: print("Error on name.")
+
+        # if self.computer_gesture_selection =="rock" or "0" : 0
+        # elif self.computer_gesture_selection =="Spock" or "1" : 1
+        # elif self.computer_gesture_selection =="paper" or "2" : 2
+        # elif self.computer_gesture_selection =="lizard" or "3" : 3
+        # elif self.computer_gesture_selection =="scissors" or "4" : 4
+        # else: print("Error on name.")
+         
+            self.computer_selection = random.randint(0,4)
+
     def player_2_gesture_selection(self):
+        # print("Player 2 enter your gesture name or number!")
+        # self.player_two_selection = input("0 - rock , 1 - spock, 2 - paper, 3 - lizard, 4 - scissors ")
         self.gesture_input = False
         self.gesture_selection = int(
             input('Player 2 choose: Rock-0, Paper-1, Scissors-2, Lizard-3, Spock-4'))
         while self.gesture_input != True:
             if self.gesture_selection == 0 or self.gesture_selection == 1 or self.gesture_selection == 2 or self.gesture_selection == 3 or self.gesture_selection == 4:
                 self.gesture_input = True
-                print('this is ok number')
             else:
                 print('Your selection was invalid')
         if self.gesture_selection == 0:
@@ -68,6 +73,20 @@ class Gestures:
             self.player_2_selection.append("Lizard")
         elif self.gesture_selection == 4:
             self.player_2_selection.append("Spock")
+
+        # if self.player_1_gesture_selection =="rock" or "0" : 0
+        # elif self.player_1_gesture_selection =="Spock" or "1" : 1
+        # elif self.player_1_gesture_selection =="paper" or "2" : 2
+        # elif self.player_1_gesture_selection =="lizard" or "3" : 3
+        # elif self.player_1_gesture_selection =="scissors" or "4" : 4
+        # else: print("Error on name.")
+
+        # if self.player_2_gesture_selection =="rock" or "0" : 0
+        # elif self.player_2_gesture_selection =="Spock" or "1" : 1
+        # elif self.player_2_gesture_selection =="paper" or "2" : 2
+        # elif self.player_2_gesture_selection =="lizard" or "3" : 3
+        # elif self.player_2_gesture_selection =="scissors" or "4" : 4
+        # else: print("Error on name.")
 
     def computer_gesture_selection(self):
         self.gesture_input = False
